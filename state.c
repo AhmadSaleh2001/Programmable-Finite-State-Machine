b865f783e993c218cedd74a0373a753dbd4d59b1
AhmadSaleh2001 <ahmadmfsaleh@gmail.com>
2025-03-01 14:34:37 +0200

    Add helper to get first empty entry

--- state.c
+++ state.c
@@ -28,6 +28,15 @@ bool is_transition_table_entry_empty(transition_table_entry_t * transition_table
     return transition_table_entry == NULL || transition_table_entry->next_state == NULL;
 }
 
+int get_first_empty_transition_table_entry(transition_table_t * transition_table) {
+    for(int i=0;i<MAX_NUMBER_OF_TRANSITION_ENTRIES;i++) {
+        if(is_transition_table_entry_empty(transition_table->transition_entries[i]))
+            return i;
+    }
+    assert(0);
+    return -1;
+}
+
 transition_table_entry_t * create_and_insert_new_transition_table_entry(
     transition_table_t * tt,
     char * state_transition_key,
@@ -37,13 +46,8 @@ transition_table_entry_t * create_and_insert_new_transition_table_entry(
     memcpy(new_transition_table_entry->state_transition_key, state_transition_key, MAX_TRANSITION_INPUT - 1);
     new_transition_table_entry->state_transition_key[MAX_TRANSITION_INPUT - 1] = '\0';    
     new_transition_table_entry->next_state = next_state;
-
-    for(int i=0;i<MAX_NUMBER_OF_TRANSITION_ENTRIES;i++) {
-        if(is_transition_table_entry_empty(tt->transition_entries[i])) {
-            tt->transition_entries[i] = new_transition_table_entry;
-            break;
-        }
-    }
+    int first_empty_entry = get_first_empty_transition_table_entry(tt);
+    tt->transition_entries[first_empty_entry] = new_transition_table_entry;
 
     return new_transition_table_entry;
 }
